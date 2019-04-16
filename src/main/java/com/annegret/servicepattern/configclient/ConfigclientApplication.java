6d7b2cabd89a1eec7948d2400421fe3cd0b955e8
Grinseteddy <annegret.kampe@arcor.de>
2019-04-16 19:55:38 +0200

    Application and controller separated

--- src/main/java/com/annegret/servicepattern/configclient/ConfigclientApplication.java
+++ src/main/java/com/annegret/servicepattern/configclient/ConfigclientApplication.java
@@ -3,13 +3,6 @@ package com.annegret.servicepattern.configclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 @SpringBootApplication
 public class ConfigclientApplication {
 
@@ -19,16 +12,3 @@ public class ConfigclientApplication {
 
 }
 
-
-@RefreshScope
-@RestController
-class MessageRestController {
-
-    @Value("${message:Hello default}")
-    private String message;
-
-    @RequestMapping("/message")
-    String getMessage() {
-        return this.message;
-    }
-}
